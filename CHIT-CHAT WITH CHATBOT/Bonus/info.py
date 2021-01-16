08210883b303db9701f2ec3a4d27e6a9349bb7bf
Sanjay Marreddi <57671048+SanjayMarreddi@users.noreply.github.com>
2021-01-16 12:03:30 +0530

    Update info.py

--- CHIT-CHAT WITH CHATBOT/Bonus/info.py
+++ CHIT-CHAT WITH CHATBOT/Bonus/info.py
@@ -1,13 +1,37 @@
-cords={}
-generic="Model_"
-for i in range(1,51):
-    for j in range(1,51):
-	    cords[generic + str(10000+(i-1)+(j-1)*50 +1)[1:]] = (i,j)
-a=""
-a=input()
-while a!="~":
+### DO NOT change or use any part of the code below. Doing so might get you disqualified.
+ 
+import random
+def info(a):
+    cords={}
+    mapp={"arduinos_trail":"Event_0001",
+        "circuital_dilemma":"Event_0021",
+        "fizzbuzz":"Event_0003",
+        "hackoverflow":"Event_0004",
+        "pandoras_box_ctf":"Event_0005",
+        "unite_for_unity":"Event_0006",
+        "frame_the_crane":"Event_0007",
+        "cool_your_engine":"Event_0008",
+        "fiducia":"Event_0009",
+        "take_charge":"Event_0010",
+        "chit_chat_with_chat_bot":"Event_0011",
+        "pythonize_everything":"Event_0012",
+        "ar_and_vr":"Event_0013",
+        "iceev_hybrid":"Event_0014",
+        "cyber_expert":"Event_0015",
+
+        }
+    generic="Event_"
+    b=[]
+    random.seed(134567)
+    ##for those nerds who might want to hardcode the SEED will be changed before evaluation so make yor code generic
+    for i in range(1,11):
+        for j in range(1,11):
+            b.append(generic + str(10000+(i-1)+(j-1)*10 +1)[1:]) 
+    random.shuffle(b)
+    for i in range(1,11):
+        for j in range(1,11):
+            cords[b[(i-1)+(j-1)*10]] = (i,j)
     try:
-        print(cords[a])
+       return(cords[mapp[a]])
     except :
-        print("Model does not exist please retry")
-    a=input()
+        return((-1,-1))
